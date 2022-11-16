fd913ecbab9308f18ece19bdbae585346792b8df
Talles Dias <tallesdelfino1@gmail.com>
2022-11-16 14:19:27 -0300

    finalizando js

--- WebStore/index.js
+++ WebStore/index.js
@@ -7,3 +7,14 @@ if (typeof localStorage.nome == "undefined") {
 nome = localStorage.nome;
 
 document.getElementById('name').innerHTML = nome;
+
+function cadastroPessoa(info) {
+    let novosDados = {
+        ...info,
+        cargo: 'programador',
+        status: 'ativo'
+    };
+    return novosDados;
+}
+
+console.log(cadastroPessoa({ nome: 'Talles', sobrenome: 'Dias' }));
